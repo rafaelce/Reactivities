5056c05950e375430f3b731186f5d15093d41b1e
Rafael Cruz <rafaeel.ce@gmail.com>
2021-12-06 15:48:54 -0300

    End of section 2

---
+++ API/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //!ativa o uso do contexto da aplicação. (SQL Server)
+            //     services.AddDbContext<DataContext>(
+            //        opt => opt.UseSqlServer(
+            //            Configuration.GetConnectionString("MSSQLContext")
+            //        )
+            //    );
+
+            //!ativa o uso do contexto da aplicação. (MySql)
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseMySql(_config.GetConnectionString("MySqlConnection"),
+                   new MySqlServerVersion(new Version()));
+            });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
