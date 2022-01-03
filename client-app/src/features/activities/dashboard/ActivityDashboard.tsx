f995a31f0753b2eb46d86c71aad013fca0edf123
Rafael Cruz <rafaeel.ce@gmail.com>
2022-01-03 14:30:44 -0300

    Creating an Activity form

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 interface Props {
@@ -15,6 +16,7 @@ export default function ActivityDashboard({ activities }: Props) {
       </Grid.Column>
       <Grid.Column width="6">
         {activities[0] && <ActivityDetails activity={activities[0]} />}
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   );
