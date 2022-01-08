a5f425f30a89d1d3b850983655f59354cc42b9d5
Rafael Cruz <rafaeel.ce@gmail.com>
2022-01-08 15:04:13 -0300

    MobX strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,17 +14,22 @@ export default class ActivityStore {
   }
 
   loadAcitvities = async () => {
-    this.loadingInitial = true;
+    this.setLoadingInitial(true);
     try {
       const activities = await agent.Activities.list();
       activities.forEach((activity) => {
         activity.date = activity.date.split("T")[0];
         this.activities.push(activity);
       });
-      this.loadingInitial = false;
+
+      this.setLoadingInitial(false);
     } catch (error) {
       console.log(error);
-      this.loadingInitial = false;
+      this.setLoadingInitial(false);
     }
+  };
+
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
   };
 }
