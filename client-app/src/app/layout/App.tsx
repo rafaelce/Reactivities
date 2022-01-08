63507ba50079e95a0f90ac19ab6dd9f3501fbc04
Rafael Cruz <rafaeel.ce@gmail.com>
2022-01-08 08:17:44 -0300

    Axios types

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -15,7 +15,14 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list().then((response) => setActivities(response));
+    agent.Activities.list().then((response) => {
+      let activities: Activity[] = [];
+      response.forEach((activity) => {
+        activity.date = activity.date.split("T")[0];
+        activities.push(activity);
+      });
+      setActivities(activities);
+    });
   }, []);
 
   function handleSelectedActivity(id: string) {
