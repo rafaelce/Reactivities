03b90d0423071e4c8b1d6f41574f4b7f839e99cc
Rafael Cruz <rafaeel.ce@gmail.com>
2022-05-07 23:24:52 -0300

    add line with comment in formik

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -7,6 +7,7 @@ import { useStore } from "../../app/stores/store";
 export default observer(function LoginForm() {
   const { userStore } = useStore();
 
+  // formik form
   return (
     <Formik
       initialValues={{ email: "", password: "", error: null }}
