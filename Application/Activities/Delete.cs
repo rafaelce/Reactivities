0eea53106b820496ec96543e1b504904553a9674
Rafael Cruz <rafaeel.ce@gmail.com>
2022-05-26 18:20:34 -0300

    Loading related data

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
 using Persistence;
 
 namespace Application.Activities
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -17,7 +17,14 @@ namespace Application.Activities
             private readonly DataContext _context;
             public Handler(DataContext context) => _context = context;
             public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
-            => Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+            {
+                var activities = await _context.Activities
+                .Include(a => a.Attendees)
+                .ThenInclude(u => u.AppUser)
+                .ToListAsync();
+
+                return Result<List<Activity>>.Success(activities);
+            }
         }
     }
 }
