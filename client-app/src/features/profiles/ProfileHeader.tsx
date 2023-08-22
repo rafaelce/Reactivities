3c6b6e7cf0fdd07bfa35971c02646cf7e18d48c5
Rafael Cruz <rafaeel.ce@gmail.com>
2023-08-22 09:22:49 -0300

    Getting the profile data part 2

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -9,17 +9,27 @@ import {
   Segment,
   Statistic,
 } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
+import { observer } from "mobx-react-lite";
 
-export default function ProfileHeader() {
+interface Props {
+  profile: Profile;
+}
+
+export default observer(function ProfileHeader({ profile }: Props) {
   return (
     <Segment>
       <Grid>
         <Grid.Column width={12}>
           <Item.Group>
             <Item>
-              <Item.Image avatar size="small" src={"/assets/user.png"} />
+              <Item.Image
+                avatar
+                size="small"
+                src={profile.image || "/assets/user.png"}
+              />
               <Item.Content verticalAlign="middle">
-                <Header as="h1" content="Displayname" />
+                <Header as="h1" content={profile.displayName} />
               </Item.Content>
             </Item>
           </Item.Group>
@@ -47,4 +57,4 @@ export default function ProfileHeader() {
       </Grid>
     </Segment>
   );
-}
+});
