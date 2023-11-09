2eb57b10790e77deb7b2238fda132ce7e91b7449
Rafael Cruz <rafaeel.ce@gmail.com>
2023-11-09 17:39:21 -0300

    Setting the main photo

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -106,6 +106,8 @@ const Profiles = {
       headers: { "Content-Type": "multipart/form-data" },
     });
   },
+  setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => requests.delete(`/photos/${id}`),
 };
 
 const agent = {
