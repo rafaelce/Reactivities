1db8f20873fa7f14ee9eb32064ffd3e9475eba02
Rafael Cruz <rafaeel.ce@gmail.com>
2023-11-20 09:26:23 -0300

    Connecting to the hub

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -41,6 +41,7 @@ namespace API.Extensions
                 {
                     policy.AllowAnyMethod()
                     .AllowAnyHeader()
+                    .AllowCredentials()
                     .WithOrigins("http://localhost:3000");
                 });
             });
