3ffa099b604978cfaf0907e83efe074fa03d391d
Rafael Cruz <rafaeel.ce@gmail.com>
2023-12-14 16:08:17 -0300

    Adding the following property to the mapping configuration

--- Application/Followers/List.cs
+++ Application/Followers/List.cs
@@ -1,4 +1,5 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
@@ -19,8 +20,10 @@ namespace Application.Followers
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-            public Handler(DataContext context, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
@@ -34,7 +37,7 @@ namespace Application.Followers
                     case "followers":
                         profiles = await _context.UserFollowings.Where(x => x.Target.UserName == request.Username)
                         .Select(u => u.Observer)
-                        .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider)
+                        .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider, new { currentUsername = _userAccessor.GetUsername() })
                         .ToListAsync();
                         break;
                     case "following":
