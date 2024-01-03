7d6a2c8ba86dee507a7ba55132d3ba247a275f12
Rafael Cruz <rafaeel.ce@gmail.com>
2024-01-03 13:47:36 -0300

    Adding filtering in the API

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
@@ -14,9 +15,10 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<PagedList<ActivityDto>>> {
-            public PagingParams Params { get; set; }
-         }
+        public class Query : IRequest<Result<PagedList<ActivityDto>>>
+        {
+            public ActivityParams Params { get; set; }
+        }
         public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
         {
             private readonly DataContext _context;
@@ -32,9 +34,17 @@ namespace Application.Activities
             public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var query = _context.Activities
+                .Where(d => d.Date >= request.Params.StartDate)
+                .OrderBy(d => d.Date)
                 .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
                 new { currentUsername = _userAccessor.GetUsername() })
                 .AsQueryable();
+
+                if (request.Params.IsGoing && !request.Params.IsHost)
+                    query = query.Where(x => x.Attendees.Any(a => a.Username == _userAccessor.GetUsername()));
+
+                if (request.Params.IsHost && !request.Params.IsGoing)
+                    query = query.Where(x => x.HostUsername == _userAccessor.GetUsername());
 
                 return Result<PagedList<ActivityDto>>.Success(
                     await PagedList<ActivityDto>.CreateAsync(query, request.Params.PageNumber,
