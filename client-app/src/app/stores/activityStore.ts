77eaad08c6edcb1fcd02af4e66a6bedb84d1372d
Rafael Cruz <rafaeel.ce@gmail.com>
2024-01-04 08:40:58 -0300

    Clien side filtering

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -12,9 +12,9 @@ export default class ActivityStore {
   editMode = false;
   loading = false;
   loadingInitial = false;
-  predicate = new Map().set("all", true);
   pagination: Pagination | null = null;
   pagingParams = new PagingParams();
+  predicate = new Map().set("all", true);
 
   constructor() {
     makeAutoObservable(this);
@@ -22,6 +22,7 @@ export default class ActivityStore {
     reaction(
       () => this.predicate.keys(),
       () => {
+        this.pagingParams = new PagingParams();
         this.activityRegistry.clear();
         this.loadActivities();
       }
@@ -32,11 +33,43 @@ export default class ActivityStore {
     this.pagingParams = pagingParams;
   };
 
+  setPredicate = (predicate: string, value: string | Date) => {
+    const resetPredicate = () => {
+      this.predicate.forEach((value, key) => {
+        if (key !== "startDate") this.predicate.delete(key);
+      });
+    };
+
+    switch (predicate) {
+      case "all":
+        resetPredicate();
+        this.predicate.set("all", true);
+        break;
+      case "isGoing":
+        resetPredicate();
+        this.predicate.set("isGoing", true);
+        break;
+      case "isHost":
+        resetPredicate();
+        this.predicate.set("isHost", true);
+        break;
+      case "startDate":
+        this.predicate.delete("startDate");
+        this.predicate.set("startDate", value);
+        break;
+    }
+  };
+
   get axiosParams() {
     const params = new URLSearchParams();
     params.append("pageNumber", this.pagingParams.pageNumber.toString());
     params.append("pageSize", this.pagingParams.pageSize.toString());
 
+    this.predicate.forEach((value, key) => {
+      if (key === "startDate")
+        params.append(key, (value as Date).toISOString());
+      else params.append(key, value);
+    });
     return params;
   }
 
