47547b25a43794b09ed0b1ff3db9e192c5fcf031
Rafael Cruz <rafaeel.ce@gmail.com>
2024-01-10 18:12:45 -0300

    Migration

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,7 +4,6 @@ import "react-calendar/dist/Calendar.css";
 import "./app/layout/styles.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import "react-datepicker/dist/react-datepicker.css";
-import reportWebVitals from "./reportWebVitals";
 import { store, StoreContext } from "./app/stores/store";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router/Routes";
@@ -15,8 +14,3 @@ ReactDOM.render(
   </StoreContext.Provider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
